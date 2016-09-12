ec34e1baf022849c74d2e4f5e4006a45e0ad7516
Nathan Feaver <nathan.feaver@gmail.com>
2016-09-11 21:58:02 -0600

    add token server

---
+++ lib/hamex/tokens_server.ex
@@ -0,0 +1,13 @@
+defmodule Hamex.TokensServer do
+  def start_link do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+  end
+
+  def tokens(tokens_server) do
+    Agent.get(tokens_server, &(&1))
+  end
+
+  def push(tokens_server, token) do
+    Agent.update(tokens_server, &([ token | &1]))
+  end
+end
